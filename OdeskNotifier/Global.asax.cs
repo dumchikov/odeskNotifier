daae003d177957d9f67fa39d635a168cc6d7ab50
Dumchikov <mdumchikov@gmail.com>
2014-05-28 00:06:41 +0300

    add logging

--- OdeskNotifier/Global.asax.cs
+++ OdeskNotifier/Global.asax.cs
@@ -12,7 +12,7 @@ namespace OdeskNotifier
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
@@ -40,6 +40,13 @@ namespace OdeskNotifier
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error()
+        {
+            var lastException = Server.GetLastError();
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Fatal(lastException);
         }
     }
 }
